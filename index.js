d5d511318a173c1f9c3adfc2300ddd2b353a0abe
Rub <melikyan740@gmail.com>
2022-11-30 17:49:12 -0800

    add Error404

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ app.get('/', (req, res)=>{
 
     res.send("Hello from index.js")
     
-
 })
 
+app.get('*',(req, res)=>{
+
+    res.status(404).send("Error404")
+
+})
 
 app.listen(process.env.PORT)
 
