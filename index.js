a8bf813995d45cf28e9b1b9b296e5433f8665f9c
Rub <melikyan740@gmail.com>
2022-11-30 18:06:01 -0800

    add controllers

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
-app.get('/', (req, res)=>{
-
-    res.send("Hello from index.js")
-    
-})
+app.use('/inventory', require('./controllers/inventory'))
 
 app.get('*',(req, res)=>{
-
+    
     res.status(404).send("Error404")
-
+    
 })
 
 app.listen(process.env.PORT)
